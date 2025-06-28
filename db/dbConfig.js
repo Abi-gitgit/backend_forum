3fc0c30be8e64869c670fa3db49f57057b346e7e
Abi-gitgit <abiadu2017@gmail.com>
2025-06-28 18:14:11 -0400

    Update dbConfig.js

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -2,10 +2,10 @@ require("dotenv").config();
 const mysql2 = require("mysql2");
 
 const dbconnection = mysql2.createPool({
-  user: process.env.USER,
-  database: process.env.DATABASE,
-  host: process.env.HOST,
-  password: process.env.PASSWORD,
+  user: process.env.DB_USER,
+  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST,
+  password: process.env.DB_PASSWORD,
   connectionLimit: 10,
 });
 
